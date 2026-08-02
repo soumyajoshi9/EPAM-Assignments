3194ee320377dc8aa8ed95e90d3ef201c571fefd
Soumya Joshi <soumya.joshii09@gmail.com>
2026-08-02 23:19:58 +0530

    Add DFS function to count valid XOR paths
    Implement DFS to count paths with XOR >= K in a tree.

---
+++ Assignment_2 /solution_1.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void dfs (int node,int parent,unordered_map<int,vector<int>> &adj,vector<int> &key,int currxor,int &ans,int K){
+
+    if (currxor >= K){
+        ans++;
+    }
+
+    for (auto child: adj[node]){
+        if (child != parent){
+            dfs(child,node,adj,key,currxor ^ key[child-1],ans,K);
+        }
+    }
+    
+}
+
+int main(){
+    int N,K;
+    cin>>N>>K;
+    int ans = 0;
+    int currxor=0;
+
+    vector<int> key;
+    for (int i=0;i<N;i++){
+        int k;
+        cin>>k;
+        key.push_back(k);
+    }
+
+    unordered_map<int,vector<int>> adj;
+
+    for (int j = 1; j < N; j++){
+        int a,b;
+        cin>>a>>b;
+
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+
+    dfs(1,1,adj,key,0,ans,K);
+    cout<<ans;
+    return 0;
+}
+
+
+
