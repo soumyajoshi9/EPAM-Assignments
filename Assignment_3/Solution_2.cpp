992427de82942837a9c88f5088af50e9c0679556
Soumya Joshi <soumya.joshii09@gmail.com>
2026-08-06 20:35:27 +0530

    Add solution for maximizing points using DP
    Implement dynamic programming solution to maximize points.

---
+++ Assignment_3/Solution_2.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(int n, vector<int> &points){
+
+    vector<int> dp(n,0);
+
+    dp[0] = points[0];
+    dp[1] = max(points[0], points[1]);
+
+    for (int i = 2; i < n; i++){
+        int include = dp[i-2] + points[i];
+        int exclude = dp[i-1] + 0;
+        dp[i] = max(include, exclude);
+    }
+
+    return dp[n-1];
+
+
+}
+
+int main(){
+    
+    int n;
+    cin >> n;
+
+    vector<int> points;
+  
+    for (int i = 0; i < n; i++){
+        int a;
+        cin >> a;
+        points.push_back(a);
+    }
+
+    int ans = solve(n, points);
+
+    cout << ans << endl;
+    
+    return 0;
+}
