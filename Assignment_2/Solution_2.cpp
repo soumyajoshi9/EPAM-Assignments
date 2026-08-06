8913d63c70dc7fdedb6e985bb75976d4fbfc0dba
Soumya Joshi <soumya.joshii09@gmail.com>
2026-08-07 01:52:25 +0530

    Solution_2

---
+++ Assignment_2/Solution_2.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int bfs(int N, int D, unordered_map<int,vector<int>> &adj){
+    
+    vector<int> dist(N+1, -1);
+    queue<int> q;
+
+    dist[1]=0;
+    q.push(1);
+
+    while(!q.empty()){
+
+        int node = q.front();
+        q.pop();
+
+        for (auto child : adj[node]){
+            if (dist[child] == -1){
+                dist[child] = dist[node]+1;
+                q.push(child);
+            }
+        }
+    }
+
+    int count = 0;
+    for (auto value : dist){
+        if(value != -1 && value <= D){
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int main(){
+    
+    int N, M, D;
+    cin >> N >> M >> D;
+
+    unordered_map<int,vector<int>> adj;
+
+    for (int i = 0; i < M; i++){
+        int a, b;
+        cin >> a >> b ;
+
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+
+    int ans = bfs(N, D, adj);
+
+    cout << ans << endl;
+    
+    return 0;
+}
